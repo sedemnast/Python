b11cc0f43e2ca5d482734ae00a027d7caed85719
Nataly <nzaitseva1799@gmail.com>
2023-04-24 17:24:40 +0300

    HW 8 added

---
+++ Homework8/phone_book.py
@@ -0,0 +1,66 @@
+# Показывает содержимое справочника
+def show_data():
+    with open('/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt', 'r', encoding='utf-8') as file:
+        book = file.read()
+    return book
+
+
+# Добавление контакта в справочник
+def new_data():
+    with open('/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt', 'a', encoding='utf-8') as file:
+        file.write(input('\nВведите данные для контакта:\n') + '\n\n')
+
+
+# Поиск в справочнике
+def find_data():
+    with open('/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt', 'r', encoding='utf-8') as file:
+        book = file.read().split('\n')
+        temp = input('Кого хотите найти? Введите данные: ')
+        for i in book:
+            if temp in i:
+                print(i)
+
+
+# Удаление данных
+def delete_contact(name):
+    with open("/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt", "r", encoding="utf-8") as file:
+        contacts = file.readlines()
+    with open("/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt", "w", encoding="utf-8") as file:
+        for contact in contacts:
+            if name not in contact:
+                file.write(contact)
+
+
+# Изменение данных
+def change_contact(new_name, old_name):
+    with open("/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt", "r", encoding="utf-8") as file:
+        contacts = file.readlines()
+    with open("/Users/sedemnast/PycharmProjects/Python/Homework8/text.txt", "w", encoding="utf-8") as file:
+        for contact in contacts:
+            if old_name in contact:
+                file.write(new_name + "\n")
+            else:
+                file.write(contact)
+
+
+while True:
+    mode = input('Выберите режим работы справочника\n'
+                 + '0 - Поиск, 1 - Увидеть содержимое справочника, '
+                   '2 - Добавить новый контакт, 3 - Удалить, 4 - Изменить, 5 - Выйти: ')
+    if mode == '1':
+        print(show_data())
+    elif mode == '0':
+        find_data()
+    elif mode == '2':
+        new_data()
+    elif mode == '3':
+        name = input('Кого нужно удалить? Введите данные: ')
+        delete_contact(name)
+    elif mode == '4':
+        old_name = input('Какой контакт надо переименовать? Введите данные: ')
+        new_name = input('Введите новое имя: ')
+        change_contact(new_name, old_name)
+    elif mode == '5':
+        break
+    else:
+        print('Не найдено. Попробуйте снова:)')
